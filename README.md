e1cd4c0874fbf1fc614acb90564a7065b79a5c50
Gerardo Miranda <gerardodev25@gmail.com>
2024-07-14 18:41:52 -0400

    chore(dev): :construction: add seed data

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ steps to follow to run the app in dev mode
 2. run database ```docker compose up -d```
 3. generate structure in the db ```yarn prisma generate```
 4. run app ```yarn dev```
+5. eject the [seed url](http://localhost:3000/api/seed) to populate the db with test data
 
 prisma commands
 ```sh
---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse, NextRequest } from 'next/server';
+import prisma from '../../lib/prisma';
+
+export async function GET(request: Request) {
+  await prisma.todo.deleteMany();
+
+  await prisma.todo.createMany({
+    data: [
+      { description: 'soul stone', complete: true },
+      { description: 'power stone' },
+      { description: 'mind stone' },
+      { description: 'reality stone' },
+      { description: 'space stone' },
+      { description: 'time stone' },
+    ],
+  });
+
+  return NextResponse.json({
+    message: 'Seed Executed',
+  });
+}
