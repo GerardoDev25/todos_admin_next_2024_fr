7bc6c7681ea56c081673e1da1746edc680998e35
Gerardo Miranda <gerardodev25@gmail.com>
2024-07-18 13:05:39 -0400

    feat(dev): :sparkles: implementing auth with auth.js

--- src/components/widget/WidgetItem.tsx
+++ src/components/widget/WidgetItem.tsx
@@ -11,7 +11,9 @@ export const WidgetItem = ({ title, children }: Props) => {
       <div className='h-full py-8 px-6 space-y-6 rounded-xl border border-gray-200 bg-white'>
         <div className='flex flex-col'>
           <h5 className='text-xl text-gray-600 text-center'>{title}</h5>
-          <div className='mt-2 flex flex-col justify-center gap-4'>{children}</div>
+          <div className='mt-2 flex flex-col justify-center gap-4'>
+            {children}
+          </div>
         </div>
       </div>
     </div>
