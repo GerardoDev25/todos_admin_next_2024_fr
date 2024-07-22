1724e54c84c781b3f809a8737eaa6f99e9410022
Gerardo Miranda <gerardodev25@gmail.com>
2024-07-22 09:50:52 -0400

    feat(dev): :sparkles: log in with next auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,14 @@
-import { handlers } from '@/auth/auth';
-export const { GET, POST } = handlers;
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GitHub from 'next-auth/providers/github';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID ?? '',
+      clientSecret: process.env.AUTH_GITHUB_SECRET ?? '',
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,24 @@
-// import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
 
-// import { auth } from '@/auth/auth';
 import { WidgetItem } from '@/components';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
 export default async function DashboardPage() {
-  // const session = await auth();
+  const session = await getServerSession(authOptions);
 
-  // if (!session?.user) {
-  //   redirect('/api/auth/signin');
-  // }
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
 
   return (
     <div>
       <div className='grid gap-6 grid-cols-1 sm:grid-cols-2'>
-        <WidgetItem title='user connected'>
+        <WidgetItem title='user connected S-side'>
           <div className='flex flex-col'>
-            {/* <span>{session.user.name}</span>
-            <span>{session.user.image}</span>
-            <span>{session.user.email}</span> */}
+            <span>{session?.user?.name}</span>
+            <span>{session?.user?.image}</span>
+            <span>{session?.user?.email}</span>
           </div>
         </WidgetItem>
       </div>
